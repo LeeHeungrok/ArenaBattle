50c44a51318102640b484e1724844a6db0c4a3f9
LeeHeungrok <rhh0208@naver.com>
2023-09-17 03:24:59 +0900

    [생성] 적 캐릭터 및 아이템 자동 생성 기능 추가

--- Source/ArenaBattle/Public/ABSection.h
+++ Source/ArenaBattle/Public/ABSection.h
@@ -38,6 +38,8 @@ private:
 	UFUNCTION()
 	void OnGateTriggerBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
+	void OnNPCSpawn();
+
 private:
 	UPROPERTY(VisibleAnywhere, Category = Mesh, Meta = (AllowPrivateAccess = true))
 	TArray<UStaticMeshComponent*> GateMeshes;
@@ -53,4 +55,13 @@ private:
 
 	UPROPERTY(EditAnywhere, Category = State, Meta = (AllowPrivateAccess = true))
 	bool bNoBattle;
+
+	UPROPERTY(EditAnywhere, Category = Spawn, Meta = (AllowPrivateAccess = true))
+	float EnemySpawnTime;
+
+	UPROPERTY(EditAnywhere, Category = Spawn, Meta = (AllowPRivateAccess = true))
+	float ItemBoxSpawnTime;
+
+	FTimerHandle SpawnNPCTimerHandle = { };
+	FTimerHandle SpawnItemBoxTimerHandle = { };
 };
